18d63aa1b6cc1938b34815b52fac1eab62ee2743
Трофимов Павел Александрович <pl8401@C201N12.pl1.edu>
2021-03-27 10:01:33 +0500

    save

---
+++ script.js
@@ -0,0 +1,50 @@
+document.onkeydown = checkButton;
+
+let cube = {
+    x: 0,
+    y: 0,
+    id: 'main'
+}
+
+
+
+function checkButton(e) {
+
+    let k = e || window.event;
+
+    if (k.keyCode === 38) {
+       //key: "ArrowUp"
+       cube.y -= 10;
+
+       render(cube.id, cube.x, cube.y);
+    }
+
+    else if (k.keyCode === 40) {
+       //key: "ArrowDown"
+       cube.y += 10;
+
+       render(cube.id, cube.x, cube.y);
+    }
+
+    else if (k.keyCode === 37) {
+        //key: "ArrowLeft"
+        cube.x -= 10;
+
+        render(cube.id, cube.x, cube.y);
+    }
+
+    else if (k.keyCode === 39) {
+        //key: "ArrowRight"
+        cube.x += 10;
+
+        render(cube.id, cube.x, cube.y);
+    }
+
+}
+
+function render(id, x, y) {
+    document.getElementById(id).style.top = y + 'px';
+    document.getElementById(id).style.left = x + 'px';
+}
+
+render(cube.id, cube.x, cube.y);
