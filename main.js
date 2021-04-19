f7a96a6d1f8a90984ee5d5ab98e2951b7fcd100f
Павел <72755708+SplitKrosGT@users.noreply.github.com>
2021-04-19 15:48:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+let cube = {
+    x: 0,
+    y: 500,
+    id: 'cube',
+    width: 46,
+    height: 93,
+    trn: .25,
+    step: 56,
+}
+
+let map = {
+    x: 0,
+    y: 0,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    color: '#f5f5f5'
+}
+
+let bots = [];
+
+let bot = {
+    x: 0,
+    y: 0,
+    id: 1,
+    width: 56,
+    height: 56,
+    trn: .25,
+    step: 56,
+    color: 'red'
+}
+
+for (let i = randomInteger(2,4); i != 0; i--) {
+    bots.push(bot);
+}
+
+renderMap(map); // Рендер карты
+renderCube(spawnCube(map, cube)); // Рендер куба
+renderBots(bots);
